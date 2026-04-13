a8e88d18fcf0e36ade415b2a6a0a0a7aefab2b68
Saeed Bohlooli <s.bohlooli@gmail.com>
2026-04-13 01:24:20 -0400

    from u107 moving cp to another project

--- ui-dashboard/src/config/appConfig.js
+++
@@ -1,9 +0,0 @@
-/**
- * Application Configuration
- * Centralized configuration for all projects and services
- */
-
-import { loadedConfig } from './configLoader'
-
-// Export the loaded configuration directly
-export default loadedConfig
